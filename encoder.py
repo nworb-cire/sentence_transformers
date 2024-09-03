9ddc528130af24a597ebaff91d756f0a01758e4c
Eric Brown <eric@ebrown.net>
2024-09-03 17:27:36 -0600

    Finish encoder

--- encoder.py
+++ encoder.py
@@ -1,33 +1,37 @@
+import json
+
 import torch
 from torch import nn
+from transformers import AutoTokenizer
 
 
 class SentenceEncoder(nn.Module):
     def __init__(
         self,
-        encoder: nn.Module | None,
         tokenizer: nn.Module,
+        d_model: int = 768,
+        num_layers: int = 4,
+        nhead: int = 4,
+        dim_feedforward: int = 3072,
     ):
         super().__init__()
-        if encoder is None:
-            self.encoder = self._default_encoder()
-        else:
-            self.encoder = encoder
         self.tokenizer = tokenizer
-
-    @staticmethod
-    def _default_encoder():
-        """Default BERT-like encoder"""
-        n_layers = 12
-        n_head = 4
-        d_model = 768
-        d_ff = 3072
-        return nn.TransformerEncoder(
-            nn.TransformerEncoderLayer(d_model, n_head, d_ff),
-            n_layers
+        self.embedding = nn.Embedding(tokenizer.vocab_size, d_model)
+        self.encoder = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(d_model, nhead, dim_feedforward, batch_first=True),
+            num_layers=num_layers,
         )
 
     def forward(self, x: list[str]) -> torch.Tensor:
         x = self.tokenizer(x, return_tensors="pt", padding=True, truncation=True)
-        x = self.encoder(x)
-        return x
+        x = self.encoder(self.embedding(x["input_ids"]), src_key_padding_mask=~x["attention_mask"].to(torch.bool))
+        return x[:, 0, :]  # return only CLS token
+
+
+if __name__ == "__main__":
+    sentences = json.load(open("sentences.json"))
+    # using this tokenizer since I will be using the model in part 2
+    tokenizer = AutoTokenizer.from_pretrained("avsolatorio/GIST-small-Embedding-v0")
+    encoder = SentenceEncoder(tokenizer)
+    x = encoder(sentences)
+    print(x.shape)
